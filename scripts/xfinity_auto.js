44c72b17a3433c9c519709a47f6538969038919c
highly5royalking-highest5 <highly5royalking@gmail.com>
2026-03-01 19:37:47 -0700

    Delete xfinity_auto.js

--- scripts/xfinity_auto.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const BASE_DIR = path.resolve(__dirname, "..");
-
-wifi.stop();
-wifi.set_ssid("Xfinity WiFi");
-const portalPath = path.join(BASE_DIR, "DarkLugia", "portals", "Xfinity", "index.html");
-wifi.start_portal(portalPath);
-display.clear();
-display.print("Portal Active: Xfinity");
